465fbaaa7a3eb8357f4c0e54cfaa3e11332f7e92
samAricha <samaricha001@gmail.com>
2023-10-31 22:57:41 +0300

    starting out on the Playlist videos section

---
+++ shared/src/androidMain/kotlin/presentation/video_player_screen/VideoPlayer.kt
@@ -0,0 +1,49 @@
+package presentation.video_player_screen
+
+import android.widget.MediaController
+import android.widget.VideoView
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+
+
+@Composable
+actual fun TrialVideoPlayer(modifier: Modifier, videoId: String) {
+
+    val ctx = LocalContext.current
+    AndroidView(factory = {
+        var view = YouTubePlayerView(it)
+        val fragment = view.addYouTubePlayerListener(
+            object : AbstractYouTubePlayerListener() {
+                override fun onReady(youTubePlayer: YouTubePlayer) {
+                    super.onReady(youTubePlayer)
+                    youTubePlayer.loadVideo(videoId, 0f)
+                }
+            }
+        )
+        view
+    })
+}
+
+
+
+@Composable
+actual fun VideoPlayer(modifier: Modifier, url: String) {
+
+    AndroidView(
+        modifier = modifier,
+        factory = { context ->
+            VideoView(context).apply {
+                setVideoPath(url)
+                val mediaController = MediaController(context)
+                mediaController.setAnchorView(this)
+                setMediaController(mediaController)
+                start()
+            }
+        },
+        update = {})
+}
